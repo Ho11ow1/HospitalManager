3a21bb3cd2314ab1686ab4c989e6f0f78c4468c2
Hollow1 <jakubvalesilver@gmail.com>
2024-11-25 00:23:16 +0100

    Resolve merge mixup in Program.cs

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -4,11 +4,7 @@ class Program
 {
     public static void Main(string[] args)
     {
-<<<<<<< HEAD
-        
-=======
         HospitalManager hospital = new HospitalManager();
         hospital.Run();
->>>>>>> Dev
     }
 }
